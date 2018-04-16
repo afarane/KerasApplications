ad05f789efc4872dd11cc7029e871cd0250db675
Abhijeet <afarane@gmail.com>
2018-04-16 16:46:19 +0530

    Update ResNet50.py

--- ResNet50/ResNet50.py
+++ ResNet50/ResNet50.py
@@ -1,16 +1,49 @@
+# ref https://deeplearningsandbox.com/how-to-build-an-image-recognition-system-using-keras-and-tensorflow-for-a-1000-everyday-object-559856e04699
 
-from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
-from keras.preprocessing import image
 import numpy as np
+import matplotlib.pyplot as plt
+from keras.preprocessing import image
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+
 
 model = ResNet50(weights='imagenet')
 
 img_path = 'Elephant.jpg'
 img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
 
-preds = model.predict(x)
-print('Predicted:', decode_predictions(preds))
-# print: [[u'n02504458', u'African_elephant']]
+
+def predict(model,img,top_n=2):	
+	x = image.img_to_array(img)
+	x = np.expand_dims(x,axis=0)
+	x = preprocess_input(x)
+	preds = model.predict(x)
+	
+	return decode_predictions(preds,top=top_n)[0]
+	
+	
+def plot_preds(image, preds):  
+
+  #image
+  plt.imshow(image)
+  plt.axis('off')
+  
+  #bar graph
+  plt.figure()  
+  order = list(reversed(range(len(preds))))  
+  bar_preds = [pr[2] for pr in preds]
+  labels = (pr[1] for pr in preds)
+  plt.barh(order, bar_preds, alpha=0.5)
+  plt.yticks(order, labels)
+  plt.xlabel('Probability')
+  plt.xlim(0, 1.01)
+  plt.tight_layout()
+  plt.show()
+
+  
+print('-----------------------------------------------------')
+preds = predict(model,img)
+print(preds)
+plot_preds(img,preds) 
+print('-----------------------------------------------------')
+
+
